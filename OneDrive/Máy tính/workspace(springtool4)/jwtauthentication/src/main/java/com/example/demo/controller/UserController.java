2c22ddedf118930325df80a136cd57b3efbc5c81
User Name <username@gmail.com>
2022-07-25 11:13:50 +0700

    app-be

---
+++ OneDrive/Máy tính/workspace(springtool4)/jwtauthentication/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostConstruct
+    public void initRoleAndUser() {
+        userService.initRoleAndUser();
+    }
+
+    @GetMapping("/registerNewUser")
+    public User registerNewUser(@RequestBody User user) {
+        return userService.registerNewUser(user);
+    }
+
+    @GetMapping({"/forAdmin"})
+    @PreAuthorize("hasRole('admin')")
+    public String forAdmin(){
+        return "This URL is only accessible to the admin";
+    }
+
+    @GetMapping({"/forUser"})
+    @PreAuthorize("hasRole('user')")
+    public String forUser(){
+        return "This URL is only accessible to the user";
+    }
+}
